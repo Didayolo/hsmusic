57c99176ac0de072d99d131f8c3b12f4f576726b
Adrien Pavao <adrien.pavao@gmail.com>
2019-06-14 23:44:05 +0200

    Update library.

--- hsmusic.py
+++ hsmusic.py
@@ -1,9 +1,36 @@
-import midi, numpy
+# Huge Symbolic Music Dataset (HSMusic)
+
+import midi
+import numpy
+import os
+import unicodedata
+import re
 
 lowerBound = 24
 upperBound = 102
+DATA_DIR = 'data'
+OUTPUT_DIR = 'output'
+
+def str_to_tag(string):
+    """ Convert a character string into another one in a standard format for naming.
+        Converts to lowercase, removes non-alpha characters, and converts spaces to hyphens.
+        Example: 'La Marseillaise' -> 'la_marseillaise'.
+    """
+    string = str(string)
+    string = unicodedata.normalize('NFKD', string) #.encode('ascii', 'ignore').decode('ascii')
+    string = re.sub('[^\w\s-]', '', string).strip().lower()
+    string = re.sub('[-\s]+', '_', string)
+    return string
+    
+def to_tags(midifile):
+    """ Read metadata from a MIDI file and return a list of tags.
+    """
+    pattern = midi.read_midifile(midifile)
+    print(pattern[0])
 
 def to_matrix(midifile):
+    """ Read a MIDI file and convert it into a binary matrix.
+    """
     pattern = midi.read_midifile(midifile)
     timeleft = [track[0].tick for track in pattern]
     posns = [0 for track in pattern]
@@ -18,12 +45,10 @@ def to_matrix(midifile):
             oldstate = state
             state = [[oldstate[x][0],0] for x in range(span)]
             statematrix.append(state)
-
         for i in range(len(timeleft)):
             while timeleft[i] == 0:
                 track = pattern[i]
                 pos = posns[i]
-
                 evt = track[pos]
                 if isinstance(evt, midi.NoteEvent):
                     if (evt.pitch < lowerBound) or (evt.pitch >= upperBound):
@@ -39,22 +64,21 @@ def to_matrix(midifile):
                         # We don't want to worry about non-4 time signatures. Bail early!
                         # print "Found time signature event {}. Bailing!".format(evt)
                         return statematrix
-
                 try:
                     timeleft[i] = track[pos + 1].tick
                     posns[i] += 1
                 except IndexError:
                     timeleft[i] = None
-
             if timeleft[i] is not None:
                 timeleft[i] -= 1
-
         if all(t is None for t in timeleft):
             break
         time += 1
     return statematrix
 
 def to_midi(statematrix, name="example"):
+    """ Write a binary matrix as a MIDI file under the filename name+'.mid'.
+    """
     statematrix = numpy.asarray(statematrix)
     pattern = midi.Pattern()
     track = midi.Track()
@@ -86,4 +110,11 @@ def to_midi(statematrix, name="example"):
         prevstate = state
     eot = midi.EndOfTrackEvent(tick=1)
     track.append(eot)
-    midi.write_midifile("{}.mid".format(name), pattern)
+    midi.write_midifile(os.path.join(OUTPUT_DIR, '{}.mid'.format(name)), pattern)
+    
+if __name__ == "__main__":
+    print('Huge Symbolic Data (HSMusic) version 0.0')
+    # TODO: Cool ASCII art
+    midiname = os.path.join(DATA_DIR, 'test.mid')
+    print(str_to_tag('Str to tag TEST /!/'))
+    print(to_tags(midiname))
